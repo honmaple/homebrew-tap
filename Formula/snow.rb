095131441d04127e9693f24f2939869e04ec85b8
honmaple <xiyang0807@gmail.com>
2024-10-09 17:43:45 +0800

    update snow version to v0.1.3

--- Formula/snow.rb
+++ Formula/snow.rb
@@ -1,8 +1,8 @@
 class Snow < Formula
   homepage "https://github.com/honmaple/snow"
   desc "static site generator"
-  url "https://github.com/honmaple/snow/releases/download/v0.1.2/snow-darwin.tar.gz"
-  sha256 "78a65c740eff24193132e063654bd1d7023f6687c94ce852e50a03a43eaba558"
+  url "https://github.com/honmaple/snow/releases/download/v0.1.3/snow-darwin.tar.gz"
+  sha256 "f2ff38f3366814a91a10a2424013c3e7d9dffec3f4ea49472e8b1e6c88ba9523"
   license "BSD-3-Clause"
 
   def install
